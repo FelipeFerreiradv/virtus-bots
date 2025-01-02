cac208cb8ac76544add9ff03c7626465001105dd
Felipe Ferreira <fefelbf@gmail.com>
2025-01-02 15:01:46 -0300

    feat: add a new params to integration with 5sim.net

--- generate-email/task.py
+++ generate-email/task.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import os
 import time
 import logging
+import cloudscraper
 
 # Configuração do logger
 logging.basicConfig(
@@ -23,132 +24,132 @@ DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME", "bot_email_generate")
 
-# Instância global do Faker
-faker = Faker()
+# # Instância global do Faker
+# faker = Faker()
 
-# Funções auxiliares
-def parse_proxy(proxy):
-    try:
-        parts = proxy.split(":")
-        if len(parts) >= 4:
-            host, port, username, password = parts[:4]
-            username = quote(username)
-            password = quote(password)
-            return {
-                "http": f"http://{username}:{password}@{host}:{port}",
-                "https": f"http://{username}:{password}@{host}:{port}",
-            }
-        raise ValueError("Formato de proxy inválido.")
-    except Exception as e:
-        logging.error(f"Erro ao processar o proxy: {e}")
-        return None
+# # Funções auxiliares
+# def parse_proxy(proxy):
+#     try:
+#         parts = proxy.split(":")
+#         if len(parts) >= 4:
+#             host, port, username, password = parts[:4]
+#             username = quote(username)
+#             password = quote(password)
+#             return {
+#                 "http": f"http://{username}:{password}@{host}:{port}",
+#                 "https": f"http://{username}:{password}@{host}:{port}",
+#             }
+#         raise ValueError("Formato de proxy inválido.")
+#     except Exception as e:
+#         logging.error(f"Erro ao processar o proxy: {e}")
+#         return None
 
-def get_proxy_info(proxy):
-    proxies = parse_proxy(proxy)
-    if not proxies:
-        return None
-    try:
-        response = requests.get("https://ipinfo.io/json", proxies=proxies, timeout=20)
-        response.raise_for_status()
-        logging.info("Conexão bem-sucedida")
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Erro ao conectar ao proxy: {e}")
-        return None
+# def get_proxy_info(proxy):
+#     proxies = parse_proxy(proxy)
+#     if not proxies:
+#         return None
+#     try:
+#         response = requests.get("https://ipinfo.io/json", proxies=proxies, timeout=20)
+#         response.raise_for_status()
+#         logging.info("Conexão bem-sucedida")
+#         return response.json()
+#     except requests.exceptions.RequestException as e:
+#         logging.error(f"Erro ao conectar ao proxy: {e}")
+#         return None
 
-def connect_db():
-    try:
-        return mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME
-        )
-    except mysql.connector.Error as e:
-        logging.error(f"Erro ao conectar ao banco de dados: {e}")
-        return None
+# def connect_db():
+#     try:
+#         return mysql.connector.connect(
+#             host=DB_HOST,
+#             user=DB_USER,
+#             password=DB_PASSWORD,
+#             database=DB_NAME
+#         )
+#     except mysql.connector.Error as e:
+#         logging.error(f"Erro ao conectar ao banco de dados: {e}")
+#         return None
 
-def save_email(db, email, senha, status_shadow):
-    try:
-        cursor = db.cursor(dictionary=True)
-        cursor.execute("SELECT COUNT(*) as count FROM emails WHERE email = %s", (email,))
-        if cursor.fetchone()['count'] == 0:
-            cursor.execute(
-                "INSERT INTO emails (email, senha, status_shadow) VALUES (%s, %s, %s)",
-                (email, senha, status_shadow)
-            )
-            db.commit()
-            logging.info(f"Email {email} salvo com status: {status_shadow}.")
-        else:
-            logging.info(f"Email {email} já existe no banco de dados.")
-    except mysql.connector.Error as e:
-        logging.error(f"Erro ao salvar email no banco de dados: {e}")
+# def save_email(db, email, senha, status_shadow):
+#     try:
+#         cursor = db.cursor(dictionary=True)
+#         cursor.execute("SELECT COUNT(*) as count FROM emails WHERE email = %s", (email,))
+#         if cursor.fetchone()['count'] == 0:
+#             cursor.execute(
+#                 "INSERT INTO emails (email, senha, status_shadow) VALUES (%s, %s, %s)",
+#                 (email, senha, status_shadow)
+#             )
+#             db.commit()
+#             logging.info(f"Email {email} salvo com status: {status_shadow}.")
+#         else:
+#             logging.info(f"Email {email} já existe no banco de dados.")
+#     except mysql.connector.Error as e:
+#         logging.error(f"Erro ao salvar email no banco de dados: {e}")
 
-def generate_emails(base_email, amount, db):
-    emails_generated = []
-    for _ in range(amount):
-        new_email = f"{base_email}{random.randint(1000, 9999)}@gmail.com"
-        senha = faker.password()
-        save_email(db, new_email, senha, "unknown")
-        emails_generated.append((new_email, senha))
-    logging.info(f"Total de emails gerados: {len(emails_generated)}")
-    return emails_generated
+# def generate_emails(base_email, amount, db):
+#     emails_generated = []
+#     for _ in range(amount):
+#         new_email = f"{base_email}{random.randint(1000, 9999)}@gmail.com"
+#         senha = faker.password()
+#         save_email(db, new_email, senha, "unknown")
+#         emails_generated.append((new_email, senha))
+#     logging.info(f"Total de emails gerados: {len(emails_generated)}")
+#     return emails_generated
 
-def generate_email_set(email_count, db):
-    email_names = [
-        "Ana", "Maria", "Beatriz", "Julia", "Gabriela", "Fernanda", "Carla", "Patricia", "Luana", "Rita",
-        "Mariana", "Catarina", "Luciana", "Marta", "Juliana", "Vanessa", "Tania", "Simone", "Isabela", "Raquel",
-        "Larissa", "Aline", "Tatiane", "Camila", "Monique", "Daniele", "Caroline", "Bianca", "Renata", "Elaine",
-        "Lúcia", "Adriana", "Sandra", "Cristiane", "Sabrina", "Lilian", "Letícia", "Rosana", "Márcia", "Sílvia",
-        "Natalia", "Priscila", "Cíntia", "Marina", "Verônica", "Michele", "Juliana", "Paula", "Kelly", "Cláudia",
-        "Ester", "Joana", "Gláucia", "Rafaela", "Gabrielle", "Luciane", "Elaine", "Mariane", "Jéssica", "Kátia",
-        "Thais", "Silvia", "Eliane", "Andreia", "Cleusa", "Vilma", "Lorena", "Roseli", "Sueli", "Neide", "Vera"
-    ]
-    email_surnames = [
-    "Silva", "Santos", "Oliveira", "Pereira", "Costa", "Almeida", "Rodrigues", "Souza", "Lima", "Gomes",
-    "Martins", "Fernandes", "Carvalho", "Melo", "Ribeiro", "Nascimento", "Araujo", "Dias", "Lopes", "Barbosa",
-    "Ferreira", "Batista", "Castro", "Pinto", "Cavalcanti", "Vieira", "Freitas", "Moreira", "Teixeira", "Machado",
-    "Queiroz", "Maciel", "Ramos", "Figueiredo", "Viana", "Moura", "Cunha", "Macedo", "Nunes", "Pereira", "Santos",
-    "Tavares", "Marques", "Brito", "Gonçalves", "Zanetti", "Serrano", "Lima", "Rosa", "Brandão", "Azevedo", 
-    "Pimentel", "Simões", "Cunha", "Barreto", "Pecanha", "Rochas", "Vasquez", "Farias", "Monteiro", "Martins",
-    "Lopes", "Morais", "Correia", "Pinheiro", "Nascimento", "Dias", "Cunha", "Lima", "Barbosa", "Silveira",
-    "Siqueira", "Salles", "Borges", "Assis", "Fonseca", "Valente", "Mota", "Fagundes", "Galvão", "Santiago", "Xavier",
-    "Vilela", "Serrano", "Vieira", "Vargas", "Ribeiro", "Moreira", "Cavalcanti", "Tavares", "Pereira", "Pimentel"
-    ]
-    emails_to_generate = []
-    for _ in range(email_count):
-        base_email = f"{random.choice(email_names)}{random.choice(email_surnames)}{random.randint(100, 999)}"
-        emails = generate_emails(base_email, 5, db)
-        emails_to_generate.extend(emails)
-    return emails_to_generate
+# def generate_email_set(email_count, db):
+#     email_names = [
+#         "Ana", "Maria", "Beatriz", "Julia", "Gabriela", "Fernanda", "Carla", "Patricia", "Luana", "Rita",
+#         "Mariana", "Catarina", "Luciana", "Marta", "Juliana", "Vanessa", "Tania", "Simone", "Isabela", "Raquel",
+#         "Larissa", "Aline", "Tatiane", "Camila", "Monique", "Daniele", "Caroline", "Bianca", "Renata", "Elaine",
+#         "Lúcia", "Adriana", "Sandra", "Cristiane", "Sabrina", "Lilian", "Letícia", "Rosana", "Márcia", "Sílvia",
+#         "Natalia", "Priscila", "Cíntia", "Marina", "Verônica", "Michele", "Juliana", "Paula", "Kelly", "Cláudia",
+#         "Ester", "Joana", "Gláucia", "Rafaela", "Gabrielle", "Luciane", "Elaine", "Mariane", "Jéssica", "Kátia",
+#         "Thais", "Silvia", "Eliane", "Andreia", "Cleusa", "Vilma", "Lorena", "Roseli", "Sueli", "Neide", "Vera"
+#     ]
+#     email_surnames = [
+#     "Silva", "Santos", "Oliveira", "Pereira", "Costa", "Almeida", "Rodrigues", "Souza", "Lima", "Gomes",
+#     "Martins", "Fernandes", "Carvalho", "Melo", "Ribeiro", "Nascimento", "Araujo", "Dias", "Lopes", "Barbosa",
+#     "Ferreira", "Batista", "Castro", "Pinto", "Cavalcanti", "Vieira", "Freitas", "Moreira", "Teixeira", "Machado",
+#     "Queiroz", "Maciel", "Ramos", "Figueiredo", "Viana", "Moura", "Cunha", "Macedo", "Nunes", "Pereira", "Santos",
+#     "Tavares", "Marques", "Brito", "Gonçalves", "Zanetti", "Serrano", "Lima", "Rosa", "Brandão", "Azevedo", 
+#     "Pimentel", "Simões", "Cunha", "Barreto", "Pecanha", "Rochas", "Vasquez", "Farias", "Monteiro", "Martins",
+#     "Lopes", "Morais", "Correia", "Pinheiro", "Nascimento", "Dias", "Cunha", "Lima", "Barbosa", "Silveira",
+#     "Siqueira", "Salles", "Borges", "Assis", "Fonseca", "Valente", "Mota", "Fagundes", "Galvão", "Santiago", "Xavier",
+#     "Vilela", "Serrano", "Vieira", "Vargas", "Ribeiro", "Moreira", "Cavalcanti", "Tavares", "Pereira", "Pimentel"
+#     ]
+#     emails_to_generate = []
+#     for _ in range(email_count):
+#         base_email = f"{random.choice(email_names)}{random.choice(email_surnames)}{random.randint(100, 999)}"
+#         emails = generate_emails(base_email, 5, db)
+#         emails_to_generate.extend(emails)
+#     return emails_to_generate
 
-def fetch_emails(db):
-    try:
-        cursor = db.cursor(dictionary=True)
-        cursor.execute("SELECT email, senha, status_shadow FROM emails WHERE status_shadow = 'unknown'")
-        emails = cursor.fetchall()
+# def fetch_emails(db):
+#     try:
+#         cursor = db.cursor(dictionary=True)
+#         cursor.execute("SELECT email, senha, status_shadow FROM emails WHERE status_shadow = 'unknown'")
+#         emails = cursor.fetchall()
 
-        for email in emails:
-            # Dividir o nome do email para garantir que o primeiro_nome e sobrenome estejam corretos
-            nome_sobrenome = email['email'].split('@')[0]
+#         for email in emails:
+#             # Dividir o nome do email para garantir que o primeiro_nome e sobrenome estejam corretos
+#             nome_sobrenome = email['email'].split('@')[0]
             
-            # Verificar se o nome_sobrenome contém um ponto
-            if '.' in nome_sobrenome:
-                nome, sobrenome = nome_sobrenome.split('.', 1)
-            else:
-                # Caso não tenha ponto, usar o mesmo valor para nome e sobrenome
-                nome = nome_sobrenome
-                sobrenome = ""
+#             # Verificar se o nome_sobrenome contém um ponto
+#             if '.' in nome_sobrenome:
+#                 nome, sobrenome = nome_sobrenome.split('.', 1)
+#             else:
+#                 # Caso não tenha ponto, usar o mesmo valor para nome e sobrenome
+#                 nome = nome_sobrenome
+#                 sobrenome = ""
 
-            email['primeiro_nome'] = nome
-            email['sobrenome'] = sobrenome
+#             email['primeiro_nome'] = nome
+#             email['sobrenome'] = sobrenome
 
-        return emails
-    except mysql.connector.Error as e:
-        logging.error(f"Erro ao buscar emails: {e}")
-        return []
+#         return emails
+#     except mysql.connector.Error as e:
+#         logging.error(f"Erro ao buscar emails: {e}")
+#         return []
 
-def get_phone_number(product="gmail", country="ru", operator="any"):
+def get_phone_number(product="google", country="russia", operator="any"):
     try:
         headers={
             "Authorization": f"Bearer {API_KEY}",
@@ -162,17 +163,18 @@ def get_phone_number(product="gmail", country="ru", operator="any"):
         }
 
         response = requests.get(
-            "https://5sim.net/v1/user/buy/activation",
+            f"https://5sim.net/v1/user/buy/hosting/{country}/{operator}/{product}",
             headers=headers, 
             params=params
         )
 
         response.raise_for_status()
         data = response.json()
+        phone = data.get('phone')
+
         logging.debug(f"Headers enviados: {headers}")
         logging.debug(f"Parametros enviados: {params}")
         logging.debug(f"Resposta completa enviados: {response}")
-        phone = data.get('phone')
         if phone:
             logging.info(f"Número de telefone adquirido: {phone}")
             return phone
@@ -183,28 +185,29 @@ def get_phone_number(product="gmail", country="ru", operator="any"):
         print(response.text)
         return None
 
-def get_verification_code(phone_number, ):
-    try:
-        for _ in range(10):
-            response = requests.get(
-                f"https://5sim.net/v1/user/buy/activation", 
-                headers={"Authorization": f"Bearer {API_KEY}"}
-            )
-            response.raise_for_status()
-            data = response.json()
-            if 'sms' in data and len(data['sms']) > 0:
-                return data['sms'][0]['code']
-            time.sleep(5)  # Esperar 5 segundos antes de tentar novamente
-        logging.warning("Código não recebido a tempo.")
-        return None
-    except Exception as e:
-        logging.error(f"Erro ao buscar código: {e}")
-        return None
+get_phone_number()
+# def get_verification_code(phone_number, ):
+#     try:
+#         for _ in range(10):
+#             response = requests.get(
+#                 f"https://5sim.net/v1/user/buy/activation", 
+#                 headers={"Authorization": f"Bearer {API_KEY}"}
+#             )
+#             response.raise_for_status()
+#             data = response.json()
+#             if 'sms' in data and len(data['sms']) > 0:
+#                 return data['sms'][0]['code']
+#             time.sleep(5)  # Esperar 5 segundos antes de tentar novamente
+#         logging.warning("Código não recebido a tempo.")
+#         return None
+#     except Exception as e:
+#         logging.error(f"Erro ao buscar código: {e}")
+#         return None
 
-if __name__ == "__main__":
-    db = connect_db()
-    if db:
-        try:
-            generate_email_set(2, db)
-        finally:
-            db.close()
+# if __name__ == "__main__":
+#     db = connect_db()
+#     if db:
+#         try:
+#             generate_email_set(2, db)
+#         finally:
+#             db.close()
