386e46a08e6d3adb95847d79be7f7aaba12a29c1
Felipe Ferreira <fefelbf@gmail.com>
2025-01-09 13:11:50 -0300

    feat: add a dynamic viewport to example.py and countries to task.py

--- generate-email/example.py
+++ generate-email/example.py
@@ -10,13 +10,12 @@ DEFAULT_WAIT_TIME = 5000
 PHONE_NUMBER_AND_ID = get_phone_number()
 
 def random_delay(min_sec=2, max_sec=5):
-    delay = random.uniform(min_sec,max_sec)
-
-    return time.sleep(delay)
+    delay = random.uniform(min_sec, max_sec)
+    time.sleep(delay)
+    return delay
 
 def random_sex():
     set_random_sex = ["Male", "Female", "Rather not say"]
-
     return random.choice(set_random_sex)
 
 class EmailAutomation:
@@ -28,12 +27,24 @@ class EmailAutomation:
                 "server": proxy,
                 "username": proxy_username,
                 "password": proxy_password
-                } if proxy else None
+            } if proxy else None
         )
 
-        self.context = self.browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+        self.context = self.browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+            viewport={"width": random.randint(1024, 1820), "height": random.randint(768, 1080)},
+            locale="en-US",
+            timezone_id="America/New_York"
+        )
         self.page = self.context.new_page()
         logging.info("Browser opened")
+
+    def simulate_typing(self, locator, text, delay_min=0.1, delay_max=0.3):
+        """Função para simular a digitação humana de forma mais realista."""
+        locator.clear()
+        for char in text:
+            locator.type(char)
+            time.sleep(random.uniform(delay_min, delay_max))
 
     def create_email_account(self, email, gmail, password):
         try:
@@ -57,7 +68,7 @@ class EmailAutomation:
 
                 self.page.wait_for_selector('input#day', timeout=DEFAULT_WAIT_TIME)
                 self.page.locator('input#day').clear()
-                self.page.fill('input#day', str(random.randint(1,30)))
+                self.page.fill('input#day', str(random.randint(1, 30)))
                 self.page.get_by_label('Month').select_option("January")
                 self.page.locator('input#year').clear()
                 self.page.fill("input#year", str(random.randint(1940, 2000)))
@@ -72,17 +83,14 @@ class EmailAutomation:
                     select_radio_email.click()
 
                 self.page.wait_for_selector("input[name='Username']", timeout=DEFAULT_WAIT_TIME)
-                self.page.locator("input[name='Username']").clear()
-                self.page.locator("input[name='Username']").fill(gmail)
+                self.simulate_typing(self.page.locator("input[name='Username']"), gmail)
                 logging.info("Input gmail filled")
                 random_delay()
                 next_button_text.click()
 
                 self.page.wait_for_selector("input[name='Passwd']", timeout=DEFAULT_WAIT_TIME)
-                self.page.locator("input[name='Passwd']").clear()
-                self.page.locator("input[name='Passwd']").fill(password)
-                self.page.locator("input[name='PasswdAgain']").clear()
-                self.page.locator("input[name='PasswdAgain']").fill(password)
+                self.simulate_typing(self.page.locator("input[name='Passwd']"), password)
+                self.simulate_typing(self.page.locator("input[name='PasswdAgain']"), password)
                 logging.info("Password filled")
                 random_delay()
                 next_button.click()
@@ -94,7 +102,7 @@ class EmailAutomation:
                 random_delay()
                 next_button.click()
 
-                self.page.wait_for_selector("input#code", timeout=10000)
+                self.page.wait_for_selector("input#code", timeout=DEFAULT_WAIT_TIME)
 
                 verification_code = get_verification_code()
 
@@ -102,31 +110,31 @@ class EmailAutomation:
                     self.page.locator("input#code").clear()
                     self.page.fill("input#code", verification_code)
                     next_button.click()
-                    logging.info(f"Verification code filled sucessfully: {verification_code}")
+                    logging.info(f"Verification code filled successfully: {verification_code}")
                 else:
                     logging.error(f"Error to fill verification code: {verification_code}")
             except PlaywrightTimeoutError as e:
                 logging.error(f"Timeout encountered: {e}")
                 return False
         except Exception as e:
-            logging.error(f"Error ao criar conta de email: {email}")
+            logging.error(f"Error creating email account: {email}")
             return False
 
     def close(self):
-        if self.driver:
-            self.driver.quit()
-            logging.info("Navegador fechado com sucesso")
+        if self.browser:
+            self.browser.close()
+            logging.info("Browser closed successfully")
 
 def main():
     db = connect_db()
     if not db:
-        logging.error("Tentativa falha ao conectar-se com o banco de dados")
+        logging.error("Failed to connect to database")
         return
     
     emails = fetch_emails(db)
 
     if not emails:              
-        logging.error("Nenhum email encontrado para o processamento")
+        logging.error("No emails found for processing")
         db.close()
         return
     
@@ -138,7 +146,7 @@ def main():
             password = entry['senha']
             gmail = entry['primeiro_nome']
             if not automation.create_email_account(email, gmail, password):
-                logging.warning(f"Tentativa de criar email {email} falhou. Prosseguindo para o próximo.")
+                logging.warning(f"Failed to create email {email}. Moving to next.")
     finally:
         if automation:
             automation.close()
